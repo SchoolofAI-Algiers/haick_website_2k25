a11ddc1d886ec492b27df7f72ee2295469c4ca62
Adel2411 <hadjarabadel.2411@gmail.com>
2025-04-29 05:48:46 +0100

    Finished 1st layout of the footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,16 @@ import Hero from "@/components/sections/Hero/Hero";
 import About from "@/components/sections/About/About";
 import AboutHaick from "@/components/sections/AboutHaick/AboutHaick";
 import Sponsor from "@/components/sections/Sponsor/Sponsor";
+import Footer from "@/components/layout/footer";
 
 export default function HomePage() {
-	return (
-		<main>
-			<Hero />
-			<About />
-			<AboutHaick />
-			<Sponsor />
-		</main>
-	);
+  return (
+    <main>
+      <Hero />
+      <About />
+      <AboutHaick />
+      <Sponsor />
+      <Footer />
+    </main>
+  );
 }
