300f62186a810301aef7fab1b674c8afabfe9eb2
imadeddineF <imadeddinefillalipro@gmail.com>
2025-05-01 04:09:56 +0100

    /

--- src/components/layout/SiteBg.tsx
+++ src/components/layout/SiteBg.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 function SiteBg() {
 	return (
 		<div className="absolute">
-			<Image src={BgShape} alt="Background Shape" className="w-full h-auto" />
+			{/* <Image src={BgShape} alt="Background Shape" className="w-full h-auto" /> */}
 		</div>
 	);
 }
