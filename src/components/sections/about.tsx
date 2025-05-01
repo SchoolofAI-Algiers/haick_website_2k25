237d8bb919caf3175f473c3ab2a4e781eafd303a
imadeddineF <imadeddinefillalipro@gmail.com>
2025-05-01 01:23:30 +0100

    [fix:aboutus] improve the component and add typing animation for the texts, ensuring its responsiveness

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -1,69 +1,147 @@
+import {
+	Bold,
+	CaseSensitive,
+	Code,
+	Image,
+	Italic,
+	Link2,
+	List,
+	ListOrdered,
+	Quote,
+} from "lucide-react";
+import Link from "next/link";
+import { TypingAnimation } from "@/components/magicui/typing-animation";
+
 const About = () => {
-	const options = ["a", "B", "I", "🖋️", "🔗", "📷", "📄", "☰"];
+	const options = [
+		CaseSensitive,
+		Bold,
+		Italic,
+		Code,
+		Link2,
+		Image,
+		Quote,
+		ListOrdered,
+		List,
+	];
+
 	return (
-		<div className="text-center py-10 px-5 bg-[#fefefe]">
+		<div className="text-center py-10 px-5">
 			<h2 className="text-[#219EBC] text-[42px] font-black mb-8">About Us</h2>
 
-			<div className="mx-auto w-[90%] max-w-[1000px] border-2 border-orange-400 rounded-lg overflow-hidden bg-white shadow-md">
-				<div className="flex gap-3 bg-[#f3f3f3] p-2.5 border-b border-orange-400 justify-start flex-wrap">
-					{options.map((option, index) => (
-						<button
+			<div className="mx-auto w-[90%] max-w-[1000px] bg-[#ECEEED] border-3 border-[#F7A209] overflow-hidden shadow-md">
+				<header className="flex gap-1 sm:gap-3 pl-4 p-2 border-b-3 border-[#F7A209] justify-start flex-wrap">
+					{options.map((Icon, index) => (
+						<Icon
 							key={index}
-							className="bg-white border border-gray-300 rounded w-8 h-8 text-sm cursor-pointer transition-colors duration-200 hover:bg-gray-200"
-						>
-							{option}
-						</button>
+							strokeWidth={3}
+							size={26}
+							className="w-6 h-6 sm:w-7 sm:h-7 cursor-pointer transition-colors duration-200 hover:bg-gray-200 p-1 rounded"
+						/>
 					))}
-				</div>
+				</header>
 
-				<div className="flex flex-wrap justify-center">
-					<div className="flex-1 basis-[300px] p-5 text-left box-border bg-[#f9f9f9] font-mono text-sm text-gray-800 border-r border-orange-400 whitespace-pre-wrap break-words">
-						<pre>
-							{`###**Who are we?**
-
-**School of AI Algiers** is a scientific club established in 2018 
-at the Higher National School of Computer Science (ESI Algiers). 
-The club unites young, motivated AI enthusiasts to help them unlock 
-their potential and enhance their skills in the field of artificial 
-intelligence. To achieve this, the club organizes **a range of 
-events,** including Haick, a datathon where participants address AI
-and data science challenges. Additionally, the club hosts
-international conferences such as **AiSummit and Indaba**, alongside
-internal events designed specifically for its members.
-(Check our website: |https://soai.netlify.app|)`}
-						</pre>
+				<div className="flex flex-col md:flex-row flex-wrap justify-center">
+					<div className="flex-1 w-full md:w-1/2 p-6 text-start text-sm text-gray-800 border-b-3 md:border-b-0 md:border-r-3 border-[#F7A209]">
+						<h3 className="text-[#219EBC] font-light text-[16px] mb-3">
+							###**Who are we?**
+						</h3>
+						<p className="font-light leading-relaxed">
+							**School of AI Algiers** is a scientific club established in 2018
+							at the Higher National School of Computer Science (ESI Algiers).
+							The club unites young, motivated AI enthusiasts to help them
+							unlock their potential and enhance their skills in the field of
+							artificial intelligence. To achieve this, the club organizes **a
+							range of events,** including Haick, a datathon where participants
+							address AI and data science challenges. Additionally, the club
+							hosts international conferences such as **AiSummit and Indaba**,
+							alongside internal events designed specifically for its members.
+							<br />
+							Check our website{" "}
+							<Link
+								href={"https://soai.netlify.app"}
+								className="font-semibold text-[#F7A209]"
+							>
+								here
+							</Link>
+						</p>
 					</div>
 
-					<div className="flex-1 basis-[300px] p-5 text-left box-border">
+					<div className="flex-1 w-full md:w-1/2 p-5 text-left box-border bg-[#FFFFFF] ">
 						<h3 className="text-xl font-bold">Who are we?</h3>
-						<p className="text-[15px] leading-relaxed my-2.5">
-							<span className="text-[#0099cc] font-bold">
+						<p className="text-[15px] leading-relaxed">
+							<TypingAnimation
+								startOnView={true}
+								duration={50}
+								delay={0}
+								className="inline text-[#219EBC] font-medium text-[15px]"
+							>
 								School of AI Algiers
-							</span>{" "}
-							is a scientific club established in 2018 at the Higher National
-							School of Computer Science (ESI Algiers). The club unites young,
-							motivated AI enthusiasts to help them unlock their potential and
-							enhance their skills in the field of artificial intelligence. To
-							achieve this, the club organizes{" "}
-							<span className="text-orange-400 font-medium">
+							</TypingAnimation>{" "}
+							<TypingAnimation
+								startOnView={true}
+								duration={20}
+								delay={1150}
+								className="inline text-[15px] leading-relaxed font-normal"
+							>
+								is a scientific club established in 2018 at the Higher National
+								School of Computer Science (ESI Algiers). The club unites young,
+								motivated AI enthusiasts to help them unlock their potential and
+								enhance their skills in the field of artificial intelligence. To
+								achieve this, the club organizes
+							</TypingAnimation>{" "}
+							<TypingAnimation
+								startOnView={true}
+								duration={50}
+								delay={7530}
+								className="inline text-[#F7A209] leading-relaxed font-medium text-[15px]"
+							>
 								a range of events
-							</span>
-							, including Haick, a datathon where participants address AI and
-							data science challenges. Additionally, the club hosts
-							international conferences such as{" "}
-							<span className="text-[#ff6600] font-bold">
-								AiSummit and Indaba
-							</span>
-							, alongside internal events designed specifically for its members.
+							</TypingAnimation>{" "}
+							<TypingAnimation
+								startOnView={true}
+								duration={20}
+								delay={8430}
+								className="inline text-[15px] leading-relaxed font-normal"
+							>
+								, including Haick, a datathon where participants address AI and
+								data science challenges. Additionally, the club hosts
+								international conferences such as
+							</TypingAnimation>{" "}
+							<TypingAnimation
+								startOnView={true}
+								duration={50}
+								delay={11990}
+								className="inline text-[15px] leading-relaxed text-[#F7A209] font-medium"
+							>
+								Ai Summit and Indaba
+							</TypingAnimation>
+							<TypingAnimation
+								startOnView={true}
+								duration={20}
+								delay={13090}
+								className="inline text-[15px] leading-relaxed font-normal"
+							>
+								, alongside internal events designed specifically for its
+								members.
+							</TypingAnimation>
 						</p>
-						<a
+
+						<Link
 							href="https://soai.netlify.app"
 							target="_blank"
 							rel="noreferrer"
-							className="text-[#007acc] underline"
+							className="text-[#219EBC] underline"
 						>
-							Check Our Website
-						</a>
+							<TypingAnimation
+								startOnView={true}
+								duration={50}
+								delay={14870}
+								className="inline text-[15px] leading-relaxed"
+							>
+								Check Our Website
+							</TypingAnimation>
+						</Link>
 					</div>
 				</div>
 			</div>
